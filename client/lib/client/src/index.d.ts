6ad6337568083810d9b1d47738b8e80240e99d3b
Microsxema <RobertGtr@protonmail.com>
2022-06-06 16:59:44 +0700

    update package

--- client/lib/client/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { UnionJobTypes } from "../../server/src/worker-job-types/types";
-import { Queue } from "bullmq";
-export declare class QueueClient {
-    private readonly connectLink;
-    private readonly redisConnection;
-    constructor(connectLink: any);
-    publisher({ name, payload }: UnionJobTypes): Promise<Queue>;
-}
