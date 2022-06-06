9b3e9bf75076477a9366d76219e3c87c49bf199d
Microsxema <RobertGtr@protonmail.com>
2022-06-06 17:28:10 +0700

    update package

--- client/lib/client/index.d.ts
+++ client/lib/client/index.d.ts
@@ -1,4 +1,5 @@
-import { UnionJobTypes } from "worker-job-types/types";
+// @ts-ignore
+import { UnionJobTypes } from "@worker-job-types/types";
 import { Queue } from "bullmq";
 export declare class QueueClient {
     private readonly connectLink;
--- client/lib/server/src/worker-job-types/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "./transaction/types";
-export * from "./statistic/types";
